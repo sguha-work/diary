8d0c6ae6fde9d2292038b40c0aa9785f3630ab0c
Sahasrangshu Guha <sahasrangshu.guha@ericsson.com>
2017-06-02 17:31:15 +0530

    work begins

--- src/pages/add-memory/add-memory.ts
+++ src/pages/add-memory/add-memory.ts
@@ -5,10 +5,32 @@ import { NavController } from 'ionic-angular';
   selector: 'page-add-memory',
   templateUrl: 'add-memory.html'
 })
+
+
 export class AddMemoryPage {
+  public todaysDate : string;
   // this tells the tabs component which Pages
   // should be each tab's root Page
+  setupBaseVariableAndPAckage() : any {
+    let today : any = new Date();
+    let dd : any = today.getDate();
+    let mm : any = today.getMonth()+1; //January is 0!
+    let yyyy : any = today.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd;
+    } 
+
+    if(mm<10) {
+        mm='0'+mm
+    } 
+
+    today = mm+'/'+dd+'/'+yyyy;
+    this.todaysDate = today;
+  }
+
   constructor(public navCtrl: NavController) {
+    this.setupBaseVariableAndPAckage()
   }
   
 }
